a378a797ec716ffb69c6e00b620ac9deea4b0565
Obianuju23 <obianujunmoh@gmail.com>
2023-03-29 08:35:31 -0700

    an empty class Square that defines a square:

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Define a class Square.
+An empty class that contains the definition of square. square is a four sided shape of which all sides are of equal length"""
+
+class Square:
+    """Represent a square."""
+    pass
+
