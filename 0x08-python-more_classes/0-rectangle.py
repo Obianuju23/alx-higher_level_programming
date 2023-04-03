556451f32f5eb417e36c862ea92fa208d07533bc
Obianuju23 <obianujunmoh@gmail.com>
2023-04-03 07:59:40 -0700

    an empty class Rectangle that defines a rectangle:

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""This class defines a reactangle."""
+
 class Rectangle:
-    """Creating an empty class Rectangle.
-    A rectangle is a quadrilateral that has its parallel sides equal to each other"""
+    """This is an empty class representing a Rectangle. 
+    A rectangle is a quadrilateral that has its parallel sides equal to each    other."""
     pass
