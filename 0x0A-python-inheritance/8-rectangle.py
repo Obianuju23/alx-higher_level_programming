1de8ce0462e331cdb89d42ea1ece07f379fc0699
Obianuju23 <obianujunmoh@gmail.com>
2023-04-11 13:54:26 -0700

    class Rectangle that inherits from BaseGeometry (7-base_geometry.py).Instantiation with width and height validated by integer_validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This function defines class Rectangle, inherited from BaseGeometry"""
-
 BaseGeometry = __import__('7-BaseGeometry').BaseGeometry
 """
 imports the super class
@@ -13,10 +12,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Instantiation of the rectangle with the objects"""
 
+        """encapsulates width and height and ensure that they are private"""
         super().integer_validator("width", width)
-        """encapsulates width and make it private"""
         self.__width = width
-
         super().integer_validator("height", height)
-        """encapsulates width and make it private"""
         self.__height = height
