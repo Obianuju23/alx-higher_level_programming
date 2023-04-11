802fa29911b213150bfc106c96a7aff411a4b371
Obianuju23 <obianujunmoh@gmail.com>
2023-04-11 14:46:50 -0700

    class Square that inherits from Rectangle (9-rectangle.py):Instantiation with size. validated by integer_validator.the area() method must be implemented

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, size):
+        """Intialize a new square.
+        Args:
+            size (int): The size of the square
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
