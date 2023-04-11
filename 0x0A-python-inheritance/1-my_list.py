2cb890bd410098911c35e12f7a56ce6c1d3e4dbe
Obianuju23 <obianujunmoh@gmail.com>
2023-04-11 07:21:36 -0700

    class MyList that inherits from list that prints the list, but sorted (ascending sort)

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,12 +5,10 @@ This program contains the print_sorted public instance method
 """
 
 """a subclass of a super class list"""
+
+
 class MyList(list):
-    
-    """initializing the subclass object"""
-    def __init__(self):
-        super().__init__()
-    
+
     def print_sorted(self):
-    """ prints an ascending sorted list"""
+        """ prints an ascending sorted list"""
         print(sorted(self))
