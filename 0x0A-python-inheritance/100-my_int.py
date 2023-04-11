8dcb1c2b4f5c12412643a78ab5a2c91700e30d78
Obianuju23 <obianujunmoh@gmail.com>
2023-04-11 15:45:29 -0700

    class MyInt that inherits from int:MyInt is a rebel. MyInt has == and != operators inverted

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,16 +4,13 @@ This is a subclass Myint that inherited from parent class int
 """
 
 
-class int:
-    pass
-
-
 class Myint(int):
     """defines a subclass Myint from a superclass int"""
-    def __eq__(self, other_objects):
+   
+    def __eq__(self, value):
         """inverts the behavior of the == and != operators"""
-        return int(self) != int(other_objects)
+        return int(self) != int(value)
 
-    def __ne__(self, other_objects):
+    def __ne__(self, value):
         """inverts the behavior of the == and != operators"""
-        return int(self) == int(other_objects)
+        return int(self) == int(value)
