c01841df5a9bc6ad2b4c63fad728ab5f8d41319d
Obianuju23 <obianujunmoh@gmail.com>
2023-04-11 13:00:12 -0700

    class Rectangle that inherits from BaseGeometry.Instantiation with width and height.width and height must be private. No getter or setter and both objects should be validated by integer_validator

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """
-This function defines an empty class
+This function defines class Rectangle, inherited from BaseGeometry
 """
-
-
-class BaseGeometry:
-    """An empty class BaseGeometry"""
+BaseGeometry = __import__('7-BaseGeometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -13,9 +10,11 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Instantiation of the rectangle with the objects"""
+
         self.integer_validator("width", width)
         """encapsulates width and make it private"""
         self.__width = width
+
         self.integer_validator("height", height)
         """encapsulates width and make it private"""
         self.__height = height
