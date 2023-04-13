352580a06991c74c24b9ee6a59b0a4ba969698ec
Obianuju23 <obianujunmoh@gmail.com>
2023-04-12 21:08:26 -0700

    Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,11 +8,10 @@ def pascal_triangle(n):
         return []
 
         triangle = [[1]]
-
         while len(triangles) != n:
             triangle = triangles[-1]
             tmp = [1]
-            for i in range(len(tri) - 1):
+            for i in range(len(triangle) - 1):
                 tmp.append(triangle[i] + triangle[i + 1])
             tmp.append(1)
             triangles.append(tmp)
