77fe0e755ebe2915a53be41c8abe563994de0347
Obianuju23 <obianujunmoh@gmail.com>
2023-04-12 21:49:13 -0700

    script that reads stdin line by line and computes metrics:

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import sys
-
+"""
+Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 def print_info():
     print('File size: {:d}'.format(file_size))
