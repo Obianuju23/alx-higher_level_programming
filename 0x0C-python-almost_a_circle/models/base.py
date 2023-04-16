eb0384f83684504ae4901cf1b5a71a65c129f41d
Obianuju23 <obianujunmoh@gmail.com>
2023-04-16 09:29:57 -0700

    created a class Base that will be the base of all other classes in the project whose aim is to ensure each product has a unique number/code attached to it

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This class represents the base of all other classes in this project
+"""
+
+
+import json 
+import csv
+ 
+
+class Base:
+    """This defines a class named Base"""
+
+    __nb_objects = 0   
+        """This is a private class attribute"""
+
+    def __init__(self, id=None):
+        """initialising the arg id"""
+        
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
