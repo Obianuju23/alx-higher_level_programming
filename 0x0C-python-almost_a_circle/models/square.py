0931a09fc856b74f887202b760f97a2509a3f508
Obianuju23 <obianujunmoh@gmail.com>
2023-04-17 07:01:56 -0700

    Square claass(subclass) interited from Rectangle (superclass) from initialization to dictionary representation of a Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """class Square from Rectangle class"""
+    """class Square(subclass)from Rectangle class"""
 
     def __init__(self, size, x=0, y=0, id=None):
 
