070031f65eb477f6f30de2e80bfbdbb2211193af
Obianuju23 <obianujunmoh@gmail.com>
2023-04-17 15:03:47 -0700

    base.py updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,12 +16,11 @@ class Base:
 
     def __init__(self, id=None):
         """initialising thepublic attribute id"""
-
-        if id is not None:
-            self.id = id
-        else:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        else:
+            self.id = id
 
     @staticmethod
     def to_json_string(list_dictionaries):
