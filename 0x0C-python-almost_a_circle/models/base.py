64ea65cdce6f4964ad2f190ba60d12e2ee37755e
Obianuju23 <obianujunmoh@gmail.com>
2023-04-17 15:12:55 -0700

    base class update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-This class represents the base of all other classes in this project
+Base class module
 """
 
 
 import json
 import csv
+"""needed for the csv files"""
 
 
 class Base:
-    """This defines a class named Base"""
+    """defines the parent or superclass"""
 
     __nb_objects = 0
-    """This is a private class attribute"""
 
     def __init__(self, id=None):
-        """initialising thepublic attribute id"""
+        """initialization of  id"""
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
@@ -24,44 +24,52 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns serialised list_dictionaries into a JSON string """
-        if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        """returns a string representation"""
+
+        m = "[]"
+        if list_dictionaries is None:
+            return m
+        if len(list_dictionaries) == 0:
+            return m
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Deserialises JSON string of list_objs to a file"""
+        """writes json to file"""
 
-        filename = cls.__ name__ + ".json"
+        filename = cls.__name__ + ".json"
         if list_objs is None:
             list_objs = []
-        with open(filename, "w", encoding="utf-8") as f:
-            f.write(cls.to_json_string([obj.to_dictionary))
-                    for obj in list_objs]))
+        with open(filename, "w", encoding="utf8") as f:
+            f.write(cls.to_json_string([obj.to_dictionary()
+                         for obj in list_objs]))
 
     @staticmethod
     def from_json_string(json_string):
-        """returns the list of JSON string representation"""
+        """returns the string from JSON"""
         if json_string is None:
-            return "[]"
+            json_string = "[]"
+        if len(json_string) == 0:
+            json_string = "[]"
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """The module returns an instance with all attribute already set"""
-        if cls.__name__ == "Square":
+        """returns an instance with all attributes set"""
+        if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        else:
+        elif cls.__name__ == "Square":
             dummy = cls(1)
+        else:
+            dummy = cls()
+
         dummy.update(**dictionary)
         return dummy
 
     @classmethod
     def load_from_file(cls):
-        """This class method returns a list of instances"""
-        filename = cls.__name__ + ".json"
+        """returns a list of instances"""
+        filename = cls.__name__+".json"
         try:
             with open(filename, "r") as f:
                 string = f.read()
@@ -73,17 +81,17 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """serialize in CSV definition"""
-        filename = cls.__name__ +".csv"
+        """serialize to CSV"""
+        filename = cls.__name__+".csv"
         if list_objs is None:
             list_objs = []
         with open(filename, "w", newline="") as f:
             for obj in list_objs:
+                wr = csv.writer(f)
                 if cls.__name__ == "Rectangle":
-                    csv.writer(f).writerow\
-               ([obj.id, obj.width, obj.height, obj.x, obj.y])
+                    wr.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
                 elif cls.__name__ == "Square":
-                    csv.writer(f).writerow([obj.id, obj.size, obj.x, obj.y])
+                    wr.writerow([obj.id, obj.size, obj.x, obj.y])
 
     @classmethod
     def load_from_file_csv(cls):
@@ -91,9 +99,8 @@ class Base:
         instances = []
         filename = cls.__name__ + ".csv"
 
-
         with open(filename, "r", newline="") as f:
-        reader = csv.reader(f)
+            reader = csv.reader(f)
             for row in reader:
                 if cls.__name__ == "Rectangle":
                     instance = {"id": int(row[0]),
