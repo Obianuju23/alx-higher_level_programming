b0698a522ad625b92921d09c77feab3ebbec6def
Obianuju23 <obianujunmoh@gmail.com>
2023-06-13 21:46:43 -0700

    script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,13 @@
 
 const Arg1 = process.argv;
 const arrArgv = Arg1.slice(2);
+
 function secondBiggest (arr) {
-  if (arr.length <= 3) {
+  if (arr.length < 2) {
     return (0);
   } else {
     arr.sort((a, b) => b - a);
-    //This sorts in ascending order
+    // This sorts in ascending order
     return (arr[1]);
   }
 }
