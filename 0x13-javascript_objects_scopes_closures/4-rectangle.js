557708d6434a36481829d23562506e093806e567
Obianuju23 <obianujunmoh@gmail.com>
2023-06-15 12:56:50 -0700

    class Rectangle that defines a rectangle: and creates some instance attributes and methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let num = 0;
+    while (num < this.height) {
+      console.log('X'.repeat(this.width));
+      num++;
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.width = width * 2;
+    this.height = height * 2;
+  }
+}
+module.exports = Rectangle;
