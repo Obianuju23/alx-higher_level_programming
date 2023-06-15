d5379fd089205b6157d5466f1d69eb9af8ce457b
Obianuju23 <obianujunmoh@gmail.com>
2023-06-15 09:08:00 -0700

    empty class Rectangle that defines a rectangle:

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // This is an Empty class definition
+}
+
+module.exports = Rectangle;
+
+//module.exports = class Rectangle { }; This canalso be used to define and export an empty at the same time
