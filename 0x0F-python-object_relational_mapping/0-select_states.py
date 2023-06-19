b14052498ee18468dbcec72c024c481c2e3e59f7
Obianuju23 <obianujunmoh@gmail.com>
2023-06-19 05:03:49 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ from sys  import argv as sysarg
 if __name__ == '__main__':
     """This means that all the code inside this block will not be executed if imported"""
     connector = MySQLdb.connect(host="localhost",
-                              port=3306, user=sysarg[1],
-                              password=sysarg[2],
-                              database=sysarg[3])
+                                port=3306, user=sysarg[1],
+                                password=sysarg[2],
+                                database=sysarg[3])
     cursor = connector.cursor()
     query  = "SELECT * FROM states ORDER id ASC;"
 
