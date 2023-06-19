4812b64e2d13256bf1a4ca1fcd0dbb7a96c8df2e
Obianuju23 <obianujunmoh@gmail.com>
2023-06-19 04:51:14 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """A python script to lists  all states from the database"""
 
-if __name__ == '__main__': 
-    """This means that all the code inside this block will not be executed if imported"""
     import MySQLdb
     from sys  import argv as sysarg
 
+if __name__ == '__main__':
+    """This means that all the code inside this block will not be executed if imported"""
     connector = MySQLdb.connect(host="localhost",
                               port=3306, user=sysarg[1],
                               password=sysarg[2],
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     for result in results:
         print(result)
     cursor.close()
-    connect.close()
+    connector.close()
