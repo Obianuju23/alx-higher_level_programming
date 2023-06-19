b60a09330fb2892ec95ea93d47d56ac477a79bd1
Obianuju23 <obianujunmoh@gmail.com>
2023-06-19 04:32:32 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A python script to lists  all states from the database"""
 
-if __name__ == '__main__: 
+if __name__ == '__main__': 
     """This means that all the code inside this block will not be executed if imported"""
     import MySQLdb
     from sys  import argv as sysarg
