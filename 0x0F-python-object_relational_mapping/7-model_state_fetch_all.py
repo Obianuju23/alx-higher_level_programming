b9c33246d7d59c85283392dd3cb6ba02a8a52d6f
Obianuju23 <obianujunmoh@gmail.com>
2023-06-19 21:49:52 -0700

    script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,7 +5,7 @@ Usage: ./7-model_state_fetch_all.py <mysql username> /
                                     <mysql password> /
                                     <database name>
 """
-import sys import argv as sysarg
+from import sys import argv as sysarg
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
