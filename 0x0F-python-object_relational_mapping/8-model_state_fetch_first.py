977a35a63b676b688829b1ca7bf630accba4582d
Obianuju23 <obianujunmoh@gmail.com>
2023-06-20 04:37:59 -0700

    script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     """This does not execute if imported"""
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(sysarg[1], sysarg[2], sysarg[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
