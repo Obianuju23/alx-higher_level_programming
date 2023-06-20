3af786dec22d0e65abafec9d9ae8984b51c5a71c
Obianuju23 <obianujunmoh@gmail.com>
2023-06-20 02:54:27 -0700

    script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """This does not execute if imported"""
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
