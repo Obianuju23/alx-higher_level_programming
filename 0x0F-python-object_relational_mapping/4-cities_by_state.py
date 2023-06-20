7953e538b1fad6546a339930370cb938652d93de
Obianuju23 <obianujunmoh@gmail.com>
2023-06-19 21:39:06 -0700

    script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,14 +9,16 @@ module, connects to the database and then list out all the cities in the DB
  if __name__ == '__main__':
    """Do not execute code when imported"""
 
-    connector = MySQLdb.connect(host='localhost', port=3306, user=sysarg[1],
+    connector = MySQLdb.connect(host="localhost", port=3306, user=sysarg[1],
                               password=sysarg[2], database=sysarg[3])
 
     cursor = connector.cursor()
-    query = "SELECT `cities`.`id` AS `cid`, `cities`.`name` AS `cname`,\
-            `states`.`name` AS `sname` FROM `cities` INNER JOIN `states`\
-            ON `cities`.`state_id` = `states`.`id` ORDER BY\
-            `cities`.`id` ASC;"
+    query = """SELECT cities.id, cities.name, states.name
+               FROM states
+               JOIN cities
+               WHERE states.id = cities.state_id
+               ORDER BY cities.id"""
+
     cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
