ed34f6f631f454eaea7ab8cdd3f5e53b469c6a3d
Obianuju23 <obianuju23@gmail.com>
2023-07-02 13:15:29 -0700

    Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #Script that takes URL,sends POST request to passed URL&display response body
-#A variable email must be sent with the value test@gmail.com
-#A variable subject must be sent with the value I will always be here for PLD
 curl -sd "email=test@gmail.com" -sd "subject=I will always be here for PLD" "$1"
