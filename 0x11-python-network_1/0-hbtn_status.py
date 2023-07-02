f12be2695dc0f2f022623c7dc50ff0a6c7b43bf5
Obianuju23 <obianuju23@gmail.com>
2023-07-02 14:08:18 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == '__main __':
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+
+print('Body response:')
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode("utf-8")))
