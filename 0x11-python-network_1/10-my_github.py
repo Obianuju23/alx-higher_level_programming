214cf43826827216dc5edb7c4768e8ab01508c45
Obianuju23 <obianuju23@gmail.com>
2023-07-02 21:48:50 -0700

    Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id, You must use Basic Authentication with a personal access token as password to access to your information (only read:user permission is needed)

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-This script that takes your github credentials,Uses the Github API 
+This script that takes your github credentials,Uses the Github API
 to display your id
 """
-from sys import sysargv
+from sys import sys.argv
 import requests
 
 
 if __name__ == "__main__":
     """prevents from being executed when imported"""
     url = "https://api.github.com/user"
-    username = sysargv[1]
-    password = sysargv[2]
+    username = sys.argv[1]
+    password = sys.argv[2]
     credentials = (username, password)
     res = requests.get(url, auth=credentials)
     res = res.json()
