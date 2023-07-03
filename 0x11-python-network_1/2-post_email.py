0ef20c0e035f98043558c0aab5d94dddd008e400
Obianuju23 <obianuju23@gmail.com>
2023-07-02 20:20:49 -0700

    Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL,sends a POST request to the passed URL,
-takes email as a parameter&displays the body of the response
+Script that takes in a URL, sends a POST request to the passed URL,
+takes email as a parameter, and displays the body of the response
 """
-import requests
+import urllib.request
 import sys
+import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data =  parse.urlencode({"email": email}).encode("ascii")
+    data = urllib.parse.urlencode({"email": email}).encode("ascii")
 
-    req = request.Request(url, data=data, method="POST")
-    with request.urlopen(req) as response:
+    req = urllib.request.Request(url, data=data, method="POST")
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode("utf-8"))
